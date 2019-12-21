e04e43e5ac4a5c2df845c95df42d55a800d91dc0
lijinwen-gz <lingwer111@163.com>
2019-12-21 23:20:16 +0800

    管理输出ok

--- src/index.js
+++ src/index.js
@@ -1,22 +1,18 @@
-import _ from 'lodash';
-import './style.css';
-import Icon from './icon.png';
-import './icon/iconfont.css';
-
+import _ from "lodash";
+import printMe from "./print.js";
 
 function component() {
-    var element = document.createElement('div');
+  var element = document.createElement("div");
+  var btn = document.createElement("button");
 
-    // Lodash（目前通过一个 script 脚本引入）对于执行这一行是必需的
-    element.innerHTML = _.join(['Hello', 'webpack111'], ' ');
-    element.classList.add('hello')
+  // Lodash（目前通过一个 script 脚本引入）对于执行这一行是必需的
+  element.innerHTML = _.join(["Hello", "webpack111"], " ");
 
-    var myIcon = new Image();
-    myIcon.src = Icon;
+  btn.innerHTML = "Click me and check the console";
+  btn.onclick = printMe;
+  element.appendChild(btn);
 
-    element.appendChild(myIcon);
-
-    return element;
+  return element;
 }
 
 document.body.appendChild(component());
